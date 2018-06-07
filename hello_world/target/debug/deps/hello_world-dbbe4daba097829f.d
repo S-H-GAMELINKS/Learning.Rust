9ed4e9cb01071dd01f64f0f229c7a3a6c0f9de8a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-07 16:23:48 +0900

    cargo build for hello_world

---
+++ hello_world/target/debug/deps/hello_world-dbbe4daba097829f.d
@@ -0,0 +1,5 @@
+C:\Users\densan004\Desktop\rust_tutorial\hello_world\target\debug\deps\hello_world-dbbe4daba097829f.exe: src\main.rs
+
+C:\Users\densan004\Desktop\rust_tutorial\hello_world\target\debug\deps\hello_world-dbbe4daba097829f.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\densan004\Desktop\rust_tutorial\hello_world\target\debug\hello_world.pdb: C:\Users\densan004\Desktop\rust_tutorial\hello_world\src\main.rs
