78c54315db30faa5871f655580d9accedcfe88fc
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-22 11:06:14 +0900

    add eat methods

---
+++ dining_philosophers/target/debug/deps/dining_philosophers-29df96b3619c5dab.d
@@ -0,0 +1,5 @@
+C:\Users\densan004\Desktop\rust\dining_philosophers\target\debug\deps\dining_philosophers-29df96b3619c5dab.exe: src\main.rs
+
+C:\Users\densan004\Desktop\rust\dining_philosophers\target\debug\deps\dining_philosophers-29df96b3619c5dab.d: src\main.rs
+
+src\main.rs:
---
+++ dining_philosophers/target/debug/dining_philosophers.d
@@ -0,0 +1,1 @@
+C:\Users\densan004\Desktop\rust\dining_philosophers\target\debug\dining_philosophers.pdb: C:\Users\densan004\Desktop\rust\dining_philosophers\src\main.rs
